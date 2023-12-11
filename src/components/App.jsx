5ed0f8f7f25ba193c18a5ad5b331ee6585c2e9c7
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-12-11 11:15:51 +0200

    Summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,76 +1,64 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import Container from "./Container.styled";
 import Contacts from "./Contacts/Contacts";
 import Form from "./Form/Form";
 import Filter from "./Filter/Filter";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+export const App = () => {
+    const [contacts, setContacts] = useState([]);
+    const [filter, setFilter] = useState('');
 
-export class App extends Component{
-    state = {
-        contacts: [],
-        filter: '',
-    }
-    componentDidMount() {
-        if (localStorage.getItem('contacts')) {
-            this.setState({contacts: JSON.parse(localStorage.getItem('contacts'))})
+    useEffect(() => { 
+        if (localStorage.getItem('contacts') && contacts.length === 0) {
+            setContacts(JSON.parse(localStorage.getItem('contacts')));
+            return;
         }
-        
-    }
-
-    componentDidUpdate(_, prevState) {
-        if (prevState.contacts.length < this.state.contacts.length) {
-            localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-        }
-        else if (prevState.contacts.length !== this.state.contacts.length) {
-            localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-        }
-    }
-  
-    addNewContact = (obj) => {
-        this.setState((prev) => { 
-            const isExist = prev.contacts.find((item) => (item.name === obj.name));
-            if (isExist) { 
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+    }, [contacts]);
+    
+    const filterContacts = contacts.filter(({ name }) => {
+                return (name.toLowerCase().includes(filter.toLowerCase()));
+    })
+    
+    const addNewContact = (obj) => {
+        setContacts((prev) => {
+            const isExist = prev.find((item) => (item.name === obj.name));
+            if (isExist) {
                 Notify.warning(`${obj.name} is already in your contacts`)
-                return;
+                return prev;
             }
             Notify.info(`New contact '${obj.name}' is successfully created`);
-            return {contacts: [...prev.contacts, obj] };
+            return [...prev, obj]
         })
     }
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value,
-        })
+
+    const handleChange = (event) => {
+        setFilter(event.target.value);
     }
-    handleDelete = (event) => {
+
+    const handleDelete = (event) => {
         const liId = event.target.parentElement.id;
-        this.setState((prev) => { 
-            const objToDelete = prev.contacts.find((item) => item.id === liId)
+        setContacts((prev) => {
+            const objToDelete = prev.find((item) => item.id === liId)
              Notify.info(`The contact '${objToDelete.name}' is successfully deleted`)
-         return {contacts: prev.contacts.filter((item) => (item.id !== liId))}
+         return prev.filter((item) => (item.id !== liId))
         })
-
-
     }
     
-    render() {
-        const filterContacts = this.state.contacts.filter(({ name }) => {
-                return (name.toLowerCase().includes(this.state.filter.toLowerCase()));
-        })
-        return(
+
+    return(
             <Container>
                 <h1>Phonebook</h1>
-                <Form addNewContact={this.addNewContact} />
+                <Form addNewContact={addNewContact} />
            
-                {this.state.contacts.length > 0 &&
+                {contacts.length > 0 &&
                     <>
                     <h2>Contacts</h2>
-                    <Filter inputInfo={this.state} handleChange={this.handleChange} />
-                    <Contacts contacts={filterContacts} handleDelete={ this.handleDelete} />
+                    <Filter inputInfo={filter} handleChange={handleChange} />
+                    <Contacts contacts={filterContacts} handleDelete={handleDelete} />
                     </>
             }
             </Container>
         )
-    }
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,41 +1,44 @@
-import { Component } from "react";
+import { useState } from "react";
 import { nanoid } from "nanoid";
 import FormStyled from "./Form.styled";
 import Button from "./Button.styled";
 import Input from "./Input.styled";
 
-class Form extends Component{
-    state = {
-        name: '',
-        number: '',
+const Form = ({addNewContact}) => {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+
+    const handleChange = (event) => {
+        if (event.target.name === 'name') {
+            setName(event.target.value);
+        }
+        else if (event.target.name === 'number') {
+            setNumber(event.target.value)
+        };
     }
- 
-    handleChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value });
-    }
-    handleSubmit = (event) => {
+
+    const handleSubmit = (event) => {
         event.preventDefault();
         const form = event.currentTarget;
         const name = form.elements.name.value;
         const num = form.elements.number.value;
         const id = nanoid();
         const obj = { name: name, number: num, id: id };
-        this.props.addNewContact(obj);
-        this.setState({ name: '', number: '' });
+        addNewContact(obj);
+        setName('');
+        setNumber('');
         form.reset();
     }
-    
 
-    render() {
-        return (
-        <FormStyled onSubmit={this.handleSubmit}>
+    return (
+        <FormStyled onSubmit={handleSubmit}>
         <label>
             Name
                     <Input
                         type="text"
                         name="name"
-                        value={this.state.name}
-                        onChange={this.handleChange}
+                        value={name}
+                        onChange={handleChange}
                         required
                         pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                         title="Name must contain only letters" />
@@ -45,8 +48,8 @@ class Form extends Component{
                     <Input
                         type="tel"
                         name="number"
-                        value={this.state.number}
-                        onChange={this.handleChange}
+                        value={number}
+                        onChange={handleChange}
                         required
                         pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
                         title="'123-45-67'   Number must contain only numbers"/>
@@ -54,7 +57,7 @@ class Form extends Component{
         <Button type="submit">Add contact</Button>
     </FormStyled>
     )
-    }
+
 }
 
 export default Form;
